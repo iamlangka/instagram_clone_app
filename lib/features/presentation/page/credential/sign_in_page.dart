629547333460cafa65b2c4f33783ac0b4e1ce7b9
iamlangka <langka@nate.com>
2022-08-28 17:59:01 +0900

    second commit

---
+++ lib/features/presentation/page/credential/sign_in_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone_app/consts.dart';
+import 'package:instagram_clone_app/features/presentation/page/credential/sign_up_page.dart';
+import 'package:instagram_clone_app/features/presentation/widgets/button_container_widget.dart';
+import 'package:instagram_clone_app/features/presentation/widgets/form_container_widget.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backGroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            Center(
+                child: SvgPicture.asset(
+              "assets/ic_instagram.svg",
+              color: primaryColor,
+            )),
+            sizeVer(30),
+            const FormContainerWidget(
+              hintText: "Email",
+            ),
+            sizeVer(15),
+            const FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            sizeVer(15),
+            ButtonContainerWidget(
+              color: blueColor,
+              text: "Sign In",
+              onTapListener: () {},
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            const Divider(
+              color: secondaryColor,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have and account? ",
+                  style: TextStyle(color: primaryColor),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    "Sign Up.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: primaryColor),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/presentation/page/credential/sign_up_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone_app/consts.dart';
+import 'package:instagram_clone_app/features/presentation/page/credential/sign_in_page.dart';
+import 'package:instagram_clone_app/features/presentation/widgets/button_container_widget.dart';
+import 'package:instagram_clone_app/features/presentation/widgets/form_container_widget.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backGroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            Center(
+                child: SvgPicture.asset(
+              "assets/ic_instagram.svg",
+              color: primaryColor,
+            )),
+            sizeVer(15),
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                    child: Image.asset("assets/profile_default.png"),
+                  ),
+                  Positioned(
+                    right: -10,
+                    bottom: -15,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.add_a_photo,
+                        color: blueColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            sizeVer(30),
+            const FormContainerWidget(
+              hintText: "Username",
+            ),
+            sizeVer(15),
+            const FormContainerWidget(
+              hintText: "Email",
+            ),
+            sizeVer(15),
+            const FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            sizeVer(15),
+            const FormContainerWidget(
+              hintText: "Bio",
+            ),
+            sizeVer(15),
+            ButtonContainerWidget(
+              color: blueColor,
+              text: "Sign Up",
+              onTapListener: () {},
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            const Divider(
+              color: secondaryColor,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Already have an account? ",
+                  style: TextStyle(color: primaryColor),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignInPage()),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    "Sign In.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: primaryColor),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
